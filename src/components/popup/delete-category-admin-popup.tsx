4318895b07e274218b3ac3d0822ea0f739da37c0
HowdyHowe <amiruddinstr@gmail.com>
2025-09-19 14:09:02 +0700

    Creating delete category, and adjuting a few widget.

--- src/components/popup/delete-category-admin-popup.tsx
+++ src/components/popup/delete-category-admin-popup.tsx
@@ -1,13 +1,13 @@
 
-export default function DeleteArticlesAdminPopup() {
+export default function DeleteCategoryAdminPopup() {
     return (
         <div className="fixed flex items-center justify-center w-full h-screen z-20">
             <div className="fixed top-0 w-full h-full bg-black opacity-50"/>
 
             <div className="absolute flex flex-col items-start justify-center w-[450px] h-[200px] px-6 bg-white rounded-lg">
 
-                <p className="text-2xl font-semibold">Delete Article</p>
-                <p className="text-[#64748B] my-4">Deleting this article is permanent and cannot be undone. All related content will be removed</p>
+                <p className="text-2xl font-semibold">Delete Category</p>
+                <p className="text-[#64748B] my-4">Delete category &quot;Technology&quot;? This will remove it from master data permanently</p>
                 <div className="flex flex-row w-full items-center justify-end gap-2">
                     <div className="flex items-center justify-center w-[100px] h-[40px] border rounded-md cursor-pointer">Cancel</div>
                     <div className="flex items-center justify-center w-[100px] h-[40px] bg-[#DC2626] text-white border rounded-md cursor-pointer">Delete</div>
