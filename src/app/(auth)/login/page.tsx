554d1b48c95ea6ed1bc0dac1579936558448cfdf
HowdyHowe <amiruddinstr@gmail.com>
2025-09-20 09:28:30 +0700

    Adding input controller using react form hook and zod.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,27 +1,52 @@
 "use client";
 
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { LuEye, LuEyeOff } from "react-icons/lu";
+
+const loginSchema = z.object({
+    username: z.string().min(5, "Username must be at least 5 letter"),
+    password: z.string().min(8, "Password must be at least 8 letter")
+})
+
+type LoginForm = z.infer<typeof loginSchema>;
 
 export default function LoginPage() {
     const [ visible, setVisible ] = useState(false);
     const router = useRouter();
 
+    const{
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<LoginForm>({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const onSubmit = (data: LoginForm) => {
+        console.log("Username: ", data.username);
+        console.log("Password: ", data.password);
+    }
+
     return (
         <div className="flex flex-col items-center justify-evenly w-[95%] h-[450px] bg-white rounded-xl lg:w-[500px] lg:h-[450px]">
-           <img src={"/images/logo-black.png"} className="w-[175px]" alt="Logo"/>
+           <img src="/images/logo-black.png" className="w-[175px]" alt="Logo"/>
 
-           <form className="flex flex-col w-[90%]">
+           <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[90%]">
                 <label className="text-lg font-semibold">Username</label>
-                <input type="text" className="w-full h-[50px] p-3 border-[2px] rounded-lg placeholder:text-[#64748B]" placeholder="Input Username"/>
+                <input type="text" {...register("username")} className="w-full h-[50px] p-3 border-[2px] rounded-lg placeholder:text-[#64748B]" placeholder="Input Username"/>
+                {/* Error sign for username */}
+                {errors.username && <p className="text-sm text-[#DC2626]">{errors.username.message}</p>}
 
                 {/* sizedbox for padding */}
                 <div className="h-[15px]"/>
 
                 <label className="text-lg font-semibold">Password</label>
                 <div className="flex flex-row items-center w-full h-[50px] border-[2px] rounded-lg">
-                    <input type={visible ? "text": "password"} className="w-full h-full p-3 bg-transparent placeholder:text-[#64748B]" placeholder="Input Password"/>
+                    <input type={visible ? "text": "password"} {...register("password")} className="w-full h-full p-3 bg-transparent placeholder:text-[#64748B]" placeholder="Input Password"/>
                     <div className="m-3" onClick={
                         () => setVisible(!visible)
                     }>
@@ -32,6 +57,8 @@ export default function LoginPage() {
                         }
                     </div>
                 </div>
+                {/* Error sign for password */}
+                {errors.password && <p className="text-sm text-[#DC2626]">{errors.password.message}</p>}
 
                 {/* sizedbox for padding */}
                 <div className="h-[30px]"/>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,27 +1,57 @@
 "use client";
 
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { LuEye, LuEyeOff } from "react-icons/lu";
+
+const signupSchema = z.object({
+    username: z.string().min(5, "Username must be at least 5 letter"),
+    password: z.string().min(5, "Password must be at least 8 letter"),
+    role: z.enum(["User", "Admin"], {
+        message: "Please select a role",
+    })
+})
+
+type SignupForm = z.infer<typeof signupSchema>
 
 export default function SignupPage() {
     const [ visible, setVisible ] = useState(false);
     const router = useRouter();
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SignupForm>({
+        resolver: zodResolver(signupSchema)
+    })
+
+    const onSubmit = (data: SignupForm) => {
+        console.log("Username: ", data.username)
+        console.log("Password: ", data.password)
+        console.log("Role: ", data.role)
+    };
+
     return (
         <div className="flex flex-col items-center justify-evenly w-[95%] h-[525px] bg-white rounded-xl lg:w-[500px] lg:h-[500px]">
             <img src={"/images/logo-black.png"} className="w-[175px]" alt="Logo"/>
 
-            <form className="flex flex-col w-[90%]">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[90%]">
                 <label className="text-lg font-semibold">Username</label>
-                <input type="text" className="w-full h-[50px] px-3 border-[2px] rounded-lg placeholder:text-[#64748B]" placeholder="Input Username"/>
+                <input type="text" {...register("username")} className="w-full h-[50px] px-3 border-[2px] rounded-lg placeholder:text-[#64748B]" placeholder="Input Username"/>
+                {/* Error sign for username */}
+                {errors.username && <p className="text-sm text-[#DC2626]">{errors.username.message}</p>}
+
 
                 {/* sizedbox for padding */}
                 <div className="h-[15px]"/>
 
                 <label className="text-lg font-semibold">Password</label>
                 <div className="flex flex-row items-center w-full h-[50px] border-[2px] rounded-lg">
-                    <input type={visible ? "text": "password"} className="w-full h-full px-3 bg-transparent placeholder:text-[#64748B]" placeholder="Input Password"/>
+                    <input type={visible ? "text": "password"} {...register("password")} className="w-full h-full px-3 bg-transparent placeholder:text-[#64748B]" placeholder="Input Password"/>
                     <div className="m-3" onClick={
                         () => setVisible(!visible)
                     }>
@@ -32,17 +62,23 @@ export default function SignupPage() {
                         }
                     </div>
                 </div>
+                {/* Error sign for password */}
+                {errors.password && <p className="text-sm text-[#DC2626]">{errors.password.message}</p>}
+
 
                 {/* sizedbox for padding */}
                 <div className="h-[15px]"/>
 
                 <label className="text-lg font-semibold">Role</label>
                 <div className="w-full h-[50px] px-3 bg-transparent border-[2px] rounded-lg">
-                    <select className="w-full h-full focus:outline-none focus:ring-0 resize-none">
+                    <select {...register("role")} className="w-full h-full focus:outline-none focus:ring-0 resize-none">
                         <option value="User">User</option>
                         <option value="Admin">Admin</option>
                     </select>
                 </div>
+                {/* Error sign for role */}
+                {errors.role && <p className="text-sm text-[#DC2626]">{errors.role.message}</p>}
+
 
                 {/* sizedbox for padding */}
                 <div className="h-[30px]"/>
